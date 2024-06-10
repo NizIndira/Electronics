3352964d9be2dfe2a687b81b77e3ea87e040b6ba
Indira <nizamutdinovaindira@gmail.com>
2024-06-10 17:50:05 +0500

    permissions added

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,41 @@
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.html import format_html
 
-# Register your models here.
+from main.models import NetworkLink, Product, Contact
+
+
+@admin.register(NetworkLink)
+class NetworkLinkAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'supplier_link', 'debt', 'tier', 'created_date',)
+    filter_horizontal = ('products',)
+    list_filter = ('category', 'supplier', 'contact__city',)
+    actions = ('clear_debt',)
+
+    def clear_debt(self, request, queryset):
+        """ Метод очищает задолженность перед поставщиком в админ-панели"""
+        for network_link in queryset:
+            network_link.debt = 0
+            network_link.save()
+        self.message_user(request, 'Задолженность перед поставщиком удалена')
+    clear_debt.short_description = "Удалить задолженность перед поставщиком"
+
+    def supplier_link(self, obj):
+        """ Метод показывает в табличной части админ-панели ссылку на поставщика"""
+        if obj.supplier:
+            link = reverse('admin:%s_%s_change' % (obj._meta.app_label, obj._meta.model_name),
+                           args=[obj.supplier.id])
+            return format_html('<a href="{}">{}</a>', link, obj.supplier.name)
+        else:
+            return "None"
+    supplier_link.short_description = "Поставщик"
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'model', 'launch_date',)
+
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('network_link', 'email', 'country',)
--- main/views.py
+++ main/views.py
@@ -2,6 +2,7 @@ from rest_framework import viewsets
 from rest_framework.generics import CreateAPIView, RetrieveAPIView, ListAPIView, UpdateAPIView, DestroyAPIView
 
 from main.models import Product, Contact, NetworkLink
+from main.permissions import IsActive
 from main.serializers import ProductSerializer, ContactSerializer, ContactListDetailSerializer, NetworkLinkSerializer, \
     NetworkLinkListViewSerializer, NetworkLinkUpdateSerializer
 
@@ -9,10 +10,12 @@ from main.serializers import ProductSerializer, ContactSerializer, ContactListDe
 class ProductViewSet(viewsets.ModelViewSet):
     serializer_class = ProductSerializer
     queryset = Product.objects.all()
+    permission_classes = [IsActive]
 
 
 class ContactViewSet(viewsets.ModelViewSet):
     queryset = Contact.objects.all()
+    permission_classes = [IsActive]
 
     default_serializer = ContactSerializer
     ListSerializers = {
@@ -26,23 +29,28 @@ class ContactViewSet(viewsets.ModelViewSet):
 
 class NetworkLinkCreateAPIView(CreateAPIView):
     serializer_class = NetworkLinkSerializer
+    permission_classes = [IsActive]
 
 
 class NetworkLinkViewAPIView(RetrieveAPIView):
     serializer_class = NetworkLinkListViewSerializer
     queryset = NetworkLink.objects.all()
+    permission_classes = [IsActive]
 
 
 class NetworkLinkListAPIView(ListAPIView):
     serializer_class = NetworkLinkListViewSerializer
     queryset = NetworkLink.objects.all()
+    permission_classes = [IsActive]
 
 
 class NetworkLinkUpdateAPIView(UpdateAPIView):
     serializer_class = NetworkLinkUpdateSerializer
     queryset = NetworkLink.objects.all()
+    permission_classes = [IsActive]
 
 
 class NetworkLinkDeleteAPIView(DestroyAPIView):
     serializer_class = NetworkLinkSerializer
     queryset = NetworkLink.objects.all()
+    permission_classes = [IsActive]
